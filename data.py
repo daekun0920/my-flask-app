aa9caa5588d692c0c3ddfaa98a08cdc79214f697
daekunhan <dosemfb0920@gmail.com>
2021-06-01 15:21:57 +0900

    my-flask-app init

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Blablabla',
+            'author': 'James Doe',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Blablabla',
+            'author': 'James Doe',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Blablabla',
+            'author': 'James Doe',
+            'create_date': '04-25-2017'
+        },
+    ]
+    return articles
